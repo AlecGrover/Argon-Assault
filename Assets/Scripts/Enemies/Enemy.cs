5d87f07faef062ef9c38e8abcfffafc77a3d15a2
AlecGrover <39810460+AlecGrover@users.noreply.github.com>
2020-06-30 17:40:07 -0700

    Explosion FX and Enemy Death
    Complete through Video 104

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        TriggerExplosionsEffects();
+        StartCoroutine(DestroyGameObject());
+    }
+
+    private void TriggerExplosionsEffects()
+    {
+        GameObject explosionEffect = transform.Find("Explosion").gameObject;
+        if (explosionEffect)
+        {
+            ParticleSystem explosion = explosionEffect.GetComponent<ParticleSystem>();
+            if (explosion)
+            {
+                explosion.Play();
+            }
+            AudioSource audioSource = explosionEffect.GetComponent<AudioSource>();
+            if (audioSource)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+
+    private IEnumerator DestroyGameObject()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    } 
+}
